c63017f5d1c1b9dfe55c7af723a709cdff011ea8
Julien <juli-colombo@hotmail.com>
2024-11-25 01:23:35 -0300

    hacer responsive el sitio

--- js/swiper.js
+++ js/swiper.js
@@ -1,12 +1,23 @@
 const swiper = new Swiper('.swiper', {
-    loop: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    // Configuración adaptable
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 30,
     },
-    slidesPerView: 1,
-  });
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+});
